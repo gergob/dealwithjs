d903c93b98fc436afcf9fe16e4fbfb46fac5eef8
Gergo Bogdan <bogdangergo@gmail.com>
2017-03-30 17:06:58 +0200

    updated RssFeedManager to use xml2js

--- src/managers/RssFeedManager.js
+++ src/managers/RssFeedManager.js
@@ -2,9 +2,12 @@
  * @flow
  */
 
+import XmlToJS from 'xml2js';
+
 export default class RssFeedManager {
   constructor(feedUrl) {
     this.feedUrl = feedUrl;
+    this.parseString = XmlToJS.parseString;
     this.parsedFeedData = null;
   }
 
@@ -12,20 +15,36 @@ export default class RssFeedManager {
     return this.feedUrl;
   }
 
-  getFeedData() {
+  getFeedData(callback, errCallback) {
     return fetch(this.feedUrl).then((resp) => {
-      console.log(resp.status);
       if(resp.status == 200) {
-        this.parsedFeedData = resp.text();
+        resp.text().then((str) => {
+          this.parseString(str, (err, data) => {
+
+            if(err && errCallback) {
+              this.parsedFeedData = null;
+              errCallback(err);
+            }
+
+            this.parsedFeedData = data;
+
+            if(callback) {
+              callback(this.parsedFeedData);
+            }
+          });
+        });
       }
       else {
         this.parsedFeedData = null;
+        if(errCallback) {
+          errCallback('Response Status is:' + resp.status);
+        }
       }
-
-      return this.parsedFeedData;
     }).catch((error) => {
-      console.error(error);
       this.parsedFeedData = null;
+      if(errCallback) {
+        errCallback(error);
+      }
     });
   }
 }
